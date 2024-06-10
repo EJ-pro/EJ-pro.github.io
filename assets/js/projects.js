183ef409ba123c3ef4a0c04a32711eed1ab95e82
EJ-pro <hsshss2522@naver.com>
2024-06-10 11:50:46 +0900

    image add & project 수정

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -20,31 +20,31 @@ let render_projects = (slug) => {
             categories: ['android', 'webdev']
         },
         {
-            image: 'assets/images/tiktok_logo.png',
-            link: 'http://tiktokmakeathon.info',
-            title: 'TikTok Make a Thon',
-            demo: 'http://tiktokmakeathon.info',
-            technologies: ['Android', 'HTML', 'JS', 'Firebase'],
-            description: "틱톡 메이커톤 홈페이지 및 관리 애플리케이션",
-            categories: ['android', 'webdev']
+            image: 'assets/images/smartpot.png',
+            link: 'https://splendid-bellflower-21f.notion.site/Smart-Pot-be8abaa5e8f14bedb1b1e2ab14c3a2ac',
+            title: 'Smart Pot',
+            demo: '',
+            technologies: ['Android', 'Kotlin', 'KaKao API', 'Python'],
+            description: "스마트 식물 관리 솔루션",
+            categories: ['android', 'server']
         },
         {
-            image: 'assets/images/tiktok_logo.png',
-            link: 'http://tiktokmakeathon.info',
-            title: 'TikTok Make a Thon',
-            demo: 'http://tiktokmakeathon.info',
-            technologies: ['Android', 'HTML', 'JS', 'Firebase'],
-            description: "틱톡 메이커톤 홈페이지 및 관리 애플리케이션",
-            categories: ['android', 'webdev']
+            image: 'assets/images/collobo.png',
+            link: '',
+            title: 'Collobo',
+            demo: '',
+            technologies: ['Android', 'Kotlin', 'Firebase'],
+            description: "",
+            categories: ['android', 'server']
         },
         {
-            image: 'assets/images/tiktok_logo.png',
-            link: 'http://tiktokmakeathon.info',
-            title: 'TikTok Make a Thon',
-            demo: 'http://tiktokmakeathon.info',
-            technologies: ['Android', 'HTML', 'JS', 'Firebase'],
-            description: "틱톡 메이커톤 홈페이지 및 관리 애플리케이션",
-            categories: ['android', 'webdev']
+            image: 'assets/images/baekjoon100day.png',
+            link: 'https://splendid-bellflower-21f.notion.site/530042c6249c4d3f9ab75f586ceac746?pvs=25',
+            title: '백준백일',
+            demo: '',
+            technologies: ['Java', 'BeakJoon'],
+            description: "",
+            categories: ['activity']
         },
     ]
 
