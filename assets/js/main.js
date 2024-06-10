3b2b1a94b0f87ed62a6144b5fb18a14776ff5a69
EJ-pro <hsshss2522@naver.com>
2024-06-10 12:07:17 +0900

    Awards 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-
 $(document).ready(function() {
     general_utils();
     blog_posts();
-})
-
+});
 
 function general_utils() {
     // smooth scrolling for nav links
@@ -11,11 +9,11 @@ function general_utils() {
     $('.extra-link a').smoothScroll();
     $('.profile-pic-link').smoothScroll();
 
-    $('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width: $(this).attr('data-percent')
-		}, 1000);
-	});
+    $('.skillbar').each(function() {
+        $(this).find('.skillbar-bar').animate({
+            width: $(this).attr('data-percent')
+        }, 1000);
+    });
 }
 
 function blog_posts() {
@@ -25,58 +23,67 @@ function blog_posts() {
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🏤 2024 산학엽협력 융합캡스톤디자인 성과발표회',
+            subtitle: 'Best Innovation Award'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🏛️ 2024 한국 콘텐츠학회 종합학술대회',
+            subtitle: 'Best Presentation Award'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🏰 2023 캡스톤디자인 내부 경진대회',
+            subtitle: 'First Place'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🛸 PEN 창업동아리 활동',
+            subtitle: 'Most Creative Idea'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '💒 대전권대학연합 산학협력 성과 공유 발표 경진대회',
+            subtitle: 'Excellence in Collaboration'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🧠 2023 캡스톤디자인 창업 Boot-Up 메이커톤',
+            subtitle: 'Best Entrepreneurial Spirit'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🎪 대전 스타트업 스쿨',
+            subtitle: 'Top Startup Award'
         },
         {
             url: 'https://splendid-bellflower-21f.notion.site/_-9288fb134c294975bce54969b0aa4182?pvs=4',
             title: '🎇 Tiktok 메이커톤',
+            subtitle: 'Best Use of Technology'
         },
     ];
 
     let post_html = [];
 
-    for(let post of posts) {
+    for (let post of posts) {
 
         let tags;
-        
-        if(post.tags) {
+
+        if (post.tags) {
             tags = post.tags.map(tag => {
-                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`
-            })
+                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`;
+            });
         }
 
         let post_template = `
         <div class="blog-post" onclick="blog_link_click('${post.url}');">
 
             <div class="blog-link">
-    
-                <h3><a href="${post.url}">${post.title}</a></h3>            
+
+                <h3><a href="${post.url}">${post.title}</a></h3>
+                ${post.subtitle ? `<p class="blog-subtitle">${post.subtitle}</p>` : ''}
 
             </div>
-    
+
             <div class="blog-goto-link">
                 <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
             </div>
