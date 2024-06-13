489b8fff9bd951a51a5d489634924841b50561e8
EJ-pro <hsshss2522@naver.com>
2024-06-13 20:59:12 +0900

    check

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,15 +36,15 @@ async function blog_posts() {
         const postSnapshot = await getDocs(postCollection);
         const posts = postSnapshot.docs.map(doc => doc.data());
 
-        console.log('Posts:', posts); // 데이터를 불러왔는지 확인
+        console.log('Posts:', posts); // Check if the data is fetched
 
         let post_html = [];
 
         for (let post of posts) {
             let post_template = `
-            <div class="blog-post" onclick="blog_link_click('#');">
+            <div class="blog-post" onclick="blog_link_click('${post['url']}');">
                 <div class="blog-link">
-                    <h3>${post['대회']}</h3>
+                    <h3><a href="${post['url']}">${post['대회']}</a></h3>
                     <p class="blog-subtitle">${post['수상']}</p>
                 </div>
                 <div class="blog-goto-link">
@@ -59,17 +59,21 @@ async function blog_posts() {
         // for the more posts link
         let more_post_template = `
         <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
+           
+           
         </div>
         `;
 
         post_html.push(more_post_template);
 
+        // Add the generated HTML to the page
         document.getElementById('rss-feeds').innerHTML = post_html.join('');
     } catch (error) {
         console.error("Error fetching Firestore data: ", error);
     }
 }
 
-function blog_link_click(url) {
-    window.location = url;
+// Ensure the function is globally accessible
+window.blog_link_click = function (url) {
+    window.location.href = url;
 }
